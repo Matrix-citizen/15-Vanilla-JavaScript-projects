d073383fdd4613bc253f79aa67e49d93139bc9ac
Rostyslav <rklyba@yahoo.com>
2021-08-14 21:15:22 +0300

    Did logic for fixed navbar and back to top button

--- 10 - Scroll/app.js
+++ 10 - Scroll/app.js
@@ -1,5 +1,5 @@
 // Element.getBoundingClientRect() method returns the size of an element and its position relative to the viewport.
-// pageYOffset is a read - only window property that returns the number of pixels the document has been scrolled vertically.
+// pageYOffset is a read-only window property that returns the number of pixels the document has been scrolled vertically.
 // slice extracts a section of a string without modifying original string
 //offsetTop - A Number, representing the top position of the element, in pixels
 
@@ -24,6 +24,26 @@ navToggle.addEventListener('click', () => {
 })
 
 // ********** fixed navbar ************
+const navbar = document.getElementById("nav");
+const topLink = document.querySelector(".top-link");
+
+window.addEventListener('scroll', () => {
+  const scrollHeight = window.pageYOffset;
+  const navHeight = navbar.getBoundingClientRect().height; 
+
+  if (scrollHeight > navHeight) {
+    navbar.classList.add('fixed-nav');
+  } else {
+    navbar.classList.remove('fixed-nav');
+  }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
+});
+
 
 
 // ********** smooth scroll ************
