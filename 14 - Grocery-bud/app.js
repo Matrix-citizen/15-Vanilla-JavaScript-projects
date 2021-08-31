50dd844c00213881bc42763a337aaf6fa73e00ee
Rostyslav <rklyba@yahoo.com>
2021-08-31 15:25:25 +0300

    Set delete item functionality

--- 14 - Grocery-bud/app.js
+++ 14 - Grocery-bud/app.js
@@ -96,8 +96,18 @@ function clearItems () {
 }
 
 // delete function
-function deleteItem() {
-  console.log('item deleted')
+function deleteItem(e) {
+  const element = e.currentTarget.parentElement.parentElement;
+  const id = element.dataset.id;
+  list.removeChild(element);
+  if (list.children.length === 0) {
+    container.classList.remove('show-container');   
+  }
+  displayAlert('item removed', 'danger');
+  setBackToDefault();
+
+  // remove from local storage
+  // removeFromLocalStorage(id);
 }
 
 // edit function
@@ -114,7 +124,30 @@ function setBackToDefault () {
 }
 // ****** LOCAL STORAGE **********
 function addToLocalStorage (id, value) {
-  console.log('added to local storage');
+  // console.log('added to local storage');
+}
+
+function removeFromLocalStorage(id) {
 
 }
 // ****** SETUP ITEMS **********
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
