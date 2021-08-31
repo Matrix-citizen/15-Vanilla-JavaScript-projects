d9c1ec2a7386154de8480894fb2316758700a3c7
Rostyslav <rklyba@yahoo.com>
2021-08-31 13:17:26 +0300

    Added display alert functionality

--- 14 - Grocery-bud/app.js
+++ 14 - Grocery-bud/app.js
@@ -27,8 +27,20 @@ function addItem (e) {
   } else if (value && editFlag) {
     console.log('editing');
   } else {
-    console.log('empty value');
+    displayAlert('please enter value', 'danger')
   }
+}
+
+// display alert
+function displayAlert (text, action) {
+  alert.textContent = text;
+  alert.classList.add(`alert-${action}`);
+
+  // remove alert
+  setTimeout(() => {
+    alert.textContent = '';
+    alert.classList.remove(`alert-${action}`)
+  }, 1000);
 }
 
 // ****** LOCAL STORAGE **********
